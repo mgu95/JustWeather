a2486598b7eb4469c5204667fe006ccdce50a9be
Mariusz Guść <mariuszgusc@outlook.com>
2024-02-04 17:05:15 +0100

    Initial commit

---
+++ src/main/java/mgu95/JustWeatherApplication.java
@@ -0,0 +1,12 @@
+package mgu95;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustWeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustWeatherApplication.class, args);
+    }
+}
